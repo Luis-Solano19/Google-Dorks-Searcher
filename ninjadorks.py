6973e3379c4a53097eb81eea1c12d9d2f52cf2de
Luis Solano Sandoval <luis.solano19@hotmail.com>
2025-02-08 00:25:43 -0600

    Integrate argparse functionality it now works with commands when executed from the command line

--- ninjadorks.py
+++ ninjadorks.py
@@ -1,8 +1,34 @@
-from dotenv import load_dotenv
+from dotenv import load_dotenv, set_key
 import os
 from googlesearch import GoogleSearch
+import argparse
+import sys
 
-def main():
+
+def env_config():
+    """
+        Configura el archivo .env con los valores proporcionados
+    """
+    api_key = input("Introduce tu API KEY: ")
+    engine_id = input("Introduce tu engine ID: ")
+
+    # En caso que .env no exista lo crea, luego crea la variable de entorno y le asigna el valor
+    set_key(".env", "GOOGLE_KEY", api_key)
+    set_key(".env", "SEARCH_ENGINE_ID", engine_id)
+
+
+def main(query, configure_env, start_page, pages, lang):
+    # Comprobar si existe el fichero/archivo de configuracion
+    env_exists = os.path.exists(".env")
+
+    if not env_exists or configure_env:
+        env_config()
+        print("Arhivo .env configurado satisfactoriamente.")
+
+        # Parar la ejecucion luego de configurar el archivo.
+        sys.exit(1)
+    
+
     # Cargar variables en el entorno
     load_dotenv()
 
@@ -12,14 +38,38 @@ def main():
     # Leer Search Engine ID
     SEARCH_ENGINE_ID = os.getenv("SEARCH_ENGINE_ID")
 
-    query = 'filetype:sql "MySQL dump" (pass|password|passwd|pwd)'
-
+    if not query:
+        print("Indica una consulta con el comando -q o --query , -h para ver la ayuda")
+        sys.exit(1)
+    
     gsearch = GoogleSearch(google_api=GOOGLE_KEY, google_engine_id=SEARCH_ENGINE_ID)
 
-    results = gsearch.search(query=query)
+    results = gsearch.search(query=query, start_page=start_page, pages=pages, lang=lang)
 
     print(results)
 
 
 if __name__ == "__main__":
-    main()
+    # Configuracion de los args del programa
+    parser = argparse.ArgumentParser(description="Esta herramienta permite realizar hacking con buscadores de forma automatica")
+
+    parser.add_argument("-q", "--query", type=str, 
+        help="Especifica el dork/consulta a buscar. \nEjemplos: -q 'League of legends' o --query  'filetype:sql \"MySQL dump\" (pass|password|passwd|pwd)' ")
+    
+    parser.add_argument("-c", "--config", action="store_true", 
+        help="Inicia el proceso de config del archivo .env \nUtiliza esta opcion sin otros argumentos para configurar las claves.")
+
+    parser.add_argument("--start_page", type=int, default=1, 
+        help="Indica la pagina de inicio del buscador para los resultados (1 default)")
+    
+    parser.add_argument("--pages", type=int,default=1, 
+        help="Indica el numero de paginas de resultados a obtener con la busqueda.")
+
+    parser.add_argument("--lang", type=str, default="lang_es", 
+        help="Codigo de idioma para los resultados de busqueda. 'lang_es' por defecto. ")
+    
+    # Procesar argumentos
+    args = parser.parse_args()
+
+
+    main(query=args.query, configure_env=args.config, start_page=args.start_page, pages=args.pages, lang=args.lang)
